e3f96cc9c892cad58fd8ef886f0dd8057d9d7f5b
mvksxm <maksimum232@gmail.com>
2026-01-31 23:09:09 -0500

    Adjusted Tester.

--- tester/tester.py
+++ tester/tester.py
@@ -4,16 +4,19 @@ class Tester:
 
     def array_test(self, test_list: list, func_to_test: Callable) -> bool:
 
-        results = [func_to_test(case[0]) == case[1] for case in test_list]
+        results = []
+        for case in test_list:
+            res = func_to_test(*case[0])
+            is_success = res == case[1]
+            results.append([is_success, res])
 
-        if not all(results):
-            print("Testing failed!")
-            failed_cnt = 0
-            for i in range(len(results)):
-                if not results[i]:
-                    failed_cnt += 1
-                    print(f"Failed Test Case -> Test Data: {test_list[i][0]}; Intended Result: {test_list[i][1]}")
+        failed_cnt = 0
+        for i in range(len(results)):
+            if not results[i][0]:
+                failed_cnt += 1
+                print(f"Failed Test Case -> Test Data: {test_list[i][0]}; Intended Result: {test_list[i][1]}; Returned Result: {results[i][1]}")
 
+        if failed_cnt > 0:
             print(f"{failed_cnt} out of {len(test_list)} tests failed!")
             return False
 
