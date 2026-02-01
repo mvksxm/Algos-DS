8787e2091339515f1e601f055b3f401eac32595c
mvksxm <maksimum232@gmail.com>
2026-01-31 23:08:51 -0500

    Revisited sorting algos.

--- algorithms/sorting/MergeSort.py
+++ algorithms/sorting/MergeSort.py
@@ -1,35 +1,7 @@
+from tester import Tester
+
 # Merge Sort
-
-def mergeSort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    left_arr = mergeSort(arr[:len(arr) // 2])
-    right_arr = mergeSort(arr[len(arr) // 2:])
-
-    return merging(left_arr, right_arr)
-
-
-def merging(a, b):
-    merged = []
-    while a and b:
-        if a[0] > b[0]:
-            merged.append(b[0])
-            b.pop(0)
-        else:
-            merged.append(a[0])
-            a.pop(0)
-
-    while a:
-        merged.append(a[0])
-        a.pop(0)
-
-    while b:
-        merged.append(b[0])
-        b.pop(0)
-
-    return merged
-
+# Time Complexity: O(log(n)*n)
 
 # [6, 7, 3, 1 ,2, 4]
 #     /   \                                 merged = []
@@ -42,7 +14,55 @@ def merging(a, b):
 #                                                 a.pop(0)
 #
 
+def _merge_arrays(left_arr, right_arr):
+    merged_arr = []
+    l_p = 0
+    r_p = 0
+
+    while l_p < len(left_arr) and r_p < len(right_arr):
+        left_elem = left_arr[l_p]
+        right_elem = right_arr[r_p]
+
+        if left_elem > right_elem:
+            merged_arr.append(right_elem)
+            r_p += 1
+        elif right_elem > left_elem:
+            merged_arr.append(left_elem)
+            l_p += 1
+        else:
+            merged_arr += [left_elem, right_elem]
+            r_p += 1
+            l_p += 1
+
+    if l_p != len(left_arr):
+        merged_arr += left_arr[l_p:len(left_arr)]
+
+    if r_p != len(right_arr):
+        merged_arr += right_arr[r_p:len(right_arr)]
+
+    return merged_arr
+
+
+def merge_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    middle_idx = len(arr) // 2
+    left_arr = arr[:middle_idx]
+    right_arr = arr[middle_idx:]
+
+    left_sorted = merge_sort(left_arr)
+    right_sorted = merge_sort(right_arr)
+
+    return _merge_arrays(left_sorted, right_sorted)
 
 if __name__ == "__main__":
-    arr = [6, 7, 3, 1, 2, 4]
-    print(mergeSort(arr))
+    tst = Tester()
+
+    test_cases = [
+        [[[6, 7, 3, 1, 2, 4]], [1, 2, 3, 4, 6, 7]],
+        [[[3, 3, 1, 1]], [1, 1, 3, 3]]
+    ]
+
+    tst.array_test(test_cases, merge_sort)
--- algorithms/sorting/QuickSort.py
+++ algorithms/sorting/QuickSort.py
@@ -1,31 +1,41 @@
-def quickSort(seq):
-    if len(seq) <= 1:
-        return seq
-
-    pivot = seq.pop()
-    elements_bigger = []
-    elements_smaller = []
-
-    for elem in seq:
-        if elem > pivot:
-            elements_bigger.append(elem)
-        else:
-            elements_smaller.append(elem)
-
-    # Recursive call explained bellow
-    return quickSort(elements_smaller) + [pivot] + quickSort(elements_bigger)
-
-
-if __name__ == "__main__":
-    seq = [4, 3, 1, 10, 11, 6]
-    print(quickSort(seq))
-
 # Quick Sort
-# [4,3,1, 10, 11, 6]
 
-# Iteration 1  Complexity O(n*log(n))
+# Example array - [4,3,1, 10, 11, 6]
+
+# Average complexity - O(log(n)*n)
+# Worst complexity - O(n^2)
+
+# Iteration 1 (Pivot - 6, arr[len(arr)-1])
 # [4, 3, 1] + [6] + [10, 11]
 #   /  \              / \
 # [1] + [4, 3]     [10] [11]
 #        /  \
 #      [3] [4]
+
+# Iteration 2 (Pivot - 4, arr[0])
+# [3, 1] + [4] + [10, 11, 6]
+#  /  \            /  |   \
+# [1] [3]        [6] [10] [11]
+
+def quick_sort(arr: list):
+
+    if not arr:
+        return []
+
+    pivot_idx = 0
+    pivot = arr[pivot_idx]
+
+    arr_smaller = [arr[i] for i in range(len(arr)) if arr[i] <= pivot and i != pivot_idx]
+    arr_bigger = [elem for elem in arr if elem > pivot]
+
+    return quick_sort(arr_smaller) + [pivot] + quick_sort(arr_bigger)
+
+
+if __name__ == "__main__":
+    arr = [4,4, 3,1, 10, 11, 6]
+    print(quick_sort(arr))
+
+# pivot - 4
+# [3, 1] + [4] + [10, 11, 6]
+# [3,1] - [1] + [3] + [] - [1,3]
+# [10, 11, 6] -  [6]([] + [6] + []) + [10]([] + [10] + []) + [11]([] + [11] + [])
