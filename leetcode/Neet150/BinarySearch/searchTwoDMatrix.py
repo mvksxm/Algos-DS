6861fe27a7357724f9712fb5eb5fb55fb4981e78
mvksxm <maksimum232@gmail.com>
2026-02-04 23:19:02 -0500

    Slight cleanup.

--- leetcode/Neet150/BinarySearch/searchTwoDMatrix.py
+++ leetcode/Neet150/BinarySearch/searchTwoDMatrix.py
@@ -44,15 +44,3 @@ class Solution:
                 r_p = middle_idx - 1
 
         return False
-
-
-
-
-
-
-
-
-
-
-# print(Solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
-# print(Solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
